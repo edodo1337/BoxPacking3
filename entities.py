534c0ed0c0541b279ae2153ea2b71948417091ce
edodo1337 <eldar100@Mail.ru>
2019-10-01 18:43:48 +0300

    init

---
+++ entities.py
@@ -0,0 +1,118 @@
+
+COLOR = 1
+count = 1
+
+positions = []
+sizes = []
+colors = []
+
+class Box:
+    position = None
+    size = None
+    color = None
+    num = None
+    def __init__(self, size):
+        global count
+        self.size = size
+        self.num = count
+        count += 1
+
+    def rotation(self, kind):
+        if (kind == 0):
+            self.size[1], self.size[0] = self.size[0], self.size[1]
+        elif (kind == 1):
+            self.rotation(0)
+            self.size[0], self.size[2] = self.size[2], self.size[0]
+        elif (kind == 2):
+            self.rotation(1)
+            self.size[1], self.size[2] = self.size[2], self.size[1]
+        elif (kind == 3):
+            self.rotation(2)
+            self.rotation(2)
+            self.rotation(1)
+        elif (kind == 4):
+            self.rotation(3)
+            self.rotation(2)
+            self.rotation(0)
+        elif (kind == 5):
+            self.rotation(4)
+            self.rotation(0)
+            self.rotation(1)
+        elif (kind == 6):
+            self.rotation(5)
+            self.rotation(0)
+            self.rotation(1)
+            self.rotation(2)
+        elif (kind == 7):
+            self.rotation(6)
+            self.rotation(0)
+            self.rotation(1)
+            self.rotation(2)
+
+
+
+
+class Container:
+    def __init__(self, l, w, h):
+        self.w = w
+        self.l = l
+        self.h = h
+        self.space = [[[0 for k in range(h)] for j in range(w)] for i in range(l)]
+
+
+    def space_print(self):
+        for i in range(self.h): #Z
+            for j in range(self.w): #Y
+                for k in range(self.l): #X
+                    print(self.space[k][j][i], end='\t')
+                print()
+            print()
+
+    def put(self, box):
+        global positions
+        global sizes
+        global colors
+
+        self.find(box)
+        if (box.position!=None):
+            for i in range(box.position[2], box.position[2]+box.size[2]): #Z
+                for j in range(box.position[1], box.position[1]+box.size[1]): #Y
+                    for k in range(box.position[0], box.position[0]+box.size[0]): #X
+                        self.space[k][j][i] = box.color
+            positions.append(box.position)
+            sizes.append(box.size)
+            colors.append("C0{}".format(box.color))
+        else:
+            print('No place')
+
+
+    def find(self, box):
+        global COLOR
+        for i in range(self.h):
+            for j in range(self.w):
+                for k in range(self.l):
+                    for ri in range(8):
+                        box.rotation(ri)
+                        if self.isFree([k, j, i], box):
+                            box.position = [k, j, i]
+                            box.color = COLOR
+                            COLOR += 1
+                            print('YES')
+                            # print(box.position)
+                            return
+                        elif (j==self.w-1) and (k==self.l-1):
+                            print(box.size, ri, [i,j,k])
+                            #box.rotation(ri)
+
+
+    def isFree(self, position, box):
+        try:
+            for i in range(position[2], position[2] + box.size[2]):  # Z
+                for j in range(position[1], position[1] + box.size[1]):  # Y
+                    for k in range(position[0], position[0] + box.size[0]):  # X
+                        if self.space[k][j][i]!=0:
+                            return False
+            return True
+        except:
+            return False
+
---
+++ main.py
@@ -0,0 +1,53 @@
+from entities import *
+from drawing import *
+
+
+
+
+
+cont = Container(5,5,5)
+
+N = 35
+max_size = 3
+boxes = []
+
+for i in range(N):
+    size = np.random.choice(range(1,max_size),3)
+    if (size[0] < size[2]):
+        size[0], size[2] = size[2], size[0]
+    if (size[1] < size[2]):
+        size[1], size[2] = size[2], size[1]
+    boxes.append(Box(size))
+
+
+#boxes.sort(key=lambda x: x.size[0]*x.size[1]*x.size[2], reverse=True)
+boxes.sort(key=lambda x: x.size[0]*x.size[1], reverse=True)
+
+
+for box in boxes:
+    cont.put(box)
+
+cont.space_print()
+
+
+
+# box1 = Box([4,5,4])
+# cont.put(box1)
+#
+# box2 = Box([5,1,2])
+# cont.put(box2)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+pc = plotCubeAt(positions, sizes, colors=colors, edgecolor="k")
+ax.add_collection3d(pc)
+
+ax.set_xlim([0, cont.l])
+ax.set_ylim([0, cont.w])
+ax.set_zlim([0, cont.h])
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.show()
