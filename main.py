120de9f5f7e8bc254907a60211cab1f554dd5bf7
Eldar <eldar100@mail.ru>
2019-11-06 17:30:37 +0300

    blocks rotation

--- main.py
+++ main.py
@@ -21,6 +21,33 @@ def read_data(filename):
 
 
 
+
+# boxdb.box_list.sort(key=lambda x: obj3D_functional(x), reverse=True)
+# boxdb.box_list.sort(key=lambda x: obj2D_functional(x), reverse=True)
+
+
+cont = Container([5, 5, 5])
+
+
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+
+
+
+
+for i in boxdb.box_list:
+    pos = find_place(cont, i)
+    if pos!=None:
+        cont.put(i, pos)
+
 boxes = []
 for i in range(2):
     #size = [random.choice(range(1,5))]*3
@@ -30,39 +57,23 @@ for i in range(2):
 for i in boxes:
     boxdb.put(i)
 
-boxdb.box_list.sort(key=lambda x: obj3D_functional(x), reverse=True)
-boxdb.box_list.sort(key=lambda x: obj2D_functional(x), reverse=True)
 
 
-cont = Container([10, 10, 5])
 
 block = Block([3,3,1], [True]*3)
+boxdb.put(block)
 block.put(boxes[0], [0,0,0])
 block.put(boxes[1], [0,1,0])
 
 
-# for ind, i in enumerate(boxes[:3]):
-#     block.put(i, [ind, 0, 0])
-# for ind, i in enumerate(boxes[3:]):
-#     block.put(i, [0, ind, 0])
-
-
-
-boxdb.put(block)
-
-
-
-box1 = Box([1, 1, 1], 5, True, [True] * 3)
-box2 = Box([1, 1, 1], 5, True, [True] * 3)
-boxdb.put(box1, box2)
 
 block.rotateY()
 
-cont.put(block, [1, 0, 0])
+position = find_place(cont, block)
+print(position)
+cont.put(block, position)
 
 
-cont.put(box1, [3, 1, 0])
-cont.put(box2, [3, 0, 0])
 
 #cont.space_print()
 
