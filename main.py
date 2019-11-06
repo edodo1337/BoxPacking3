38addea6952de8300b649c507859a2f228fe4ad3
Eldar <eldar100@mail.ru>
2019-11-06 17:48:57 +0300

    blocks rotation

--- main.py
+++ main.py
@@ -26,27 +26,24 @@ def read_data(filename):
 # boxdb.box_list.sort(key=lambda x: obj2D_functional(x), reverse=True)
 
 
-cont = Container([5, 5, 5])
+cont = Container([7, 7, 5])
 
 
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
-boxdb.put(Box([1, 1, 1], 5, True, [True] * 3))
+boxdb.put(Box([2, 2, 2], 5, True, [True] * 3))
+boxdb.put(Box([2, 2, 2], 5, True, [True] * 3))
+boxdb.put(Box([2, 2, 2], 5, True, [True] * 3))
+boxdb.put(Box([2, 2, 2], 5, True, [True] * 3))
+boxdb.put(Box([2, 2, 2], 5, True, [True] * 3))
+boxdb.put(Box([2, 2, 2], 5, True, [True] * 3))
 
 
 
 
-for i in boxdb.box_list:
-    pos = find_place(cont, i)
-    if pos!=None:
-        cont.put(i, pos)
+
+# for i in boxdb.box_list:
+#     pos = find_place(cont, i)
+#     if pos!=None:
+#         cont.put(i, pos)
 
 boxes = []
 for i in range(2):
@@ -54,24 +51,28 @@ for i in range(2):
     size = [2,1,1]
     boxes.append(Box(size, 5, True, [True, True, True]))
 
+boxes.append(Box([1,1,1], 5, True, [True, True, True]))
+
+
 for i in boxes:
     boxdb.put(i)
 
-
-
-
-block = Block([3,3,1], [True]*3)
+block = Block([3, 3, 1], [True] * 3)
 boxdb.put(block)
-block.put(boxes[0], [0,0,0])
-block.put(boxes[1], [0,1,0])
+block.put(boxes[0], [0, 0, 0])
+block.put(boxes[1], [0, 1, 0])
+block.put(boxes[2], [2, 0, 0])
+
+
+block.rotateZ()
+block.rotateX()
 
 
 
-block.rotateY()
 
 position = find_place(cont, block)
 print(position)
-cont.put(block, position)
+cont.put(block, [1,1,1])
 
 
 
