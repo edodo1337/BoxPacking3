81804c346f64e35b886a85638d84efc63aa5495d
eldar100 <eldar100@gitlab.com>
2020-03-19 18:29:00 +0300

    fixes

--- libgls.cpp
+++ libgls.cpp
@@ -95,43 +95,43 @@ extern "C" bool check_line_face_intersect(int line[4], int face[4])
     }
 }
 
-std::array<bool,3>* check_boxes_intersect(AbstractBox box1, AbstractBox box2)
-{   
-    std::array<int,3> pos1 = box1.get_position();
-    std::array<int,3> pos2 = box2.get_position();
-    std::array<int,3> diag1 = box1.get_diag();
-    std::array<int,3> diag2 = box2.get_diag();
-    std::array<int,3> size1 = box1.get_size();
-    std::array<int,3> size2 = box2.get_size();
-    static std::array<bool,3> checkXYZ{false, false, false};
+// std::array<bool,3>* check_boxes_intersect(AbstractBox box1, AbstractBox box2)
+// {   
+//     std::array<int,3> pos1 = box1.get_position();
+//     std::array<int,3> pos2 = box2.get_position();
+//     std::array<int,3> diag1 = box1.get_diag();
+//     std::array<int,3> diag2 = box2.get_diag();
+//     std::array<int,3> size1 = box1.get_size();
+//     std::array<int,3> size2 = box2.get_size();
+//     static std::array<bool,3> checkXYZ{false, false, false};
 
-    for (int i(0); i<3; i++)
-    {
-        if (diag1[i] < 0)
-            pos1[i] = diag1[i] + pos1[i];
+//     for (int i(0); i<3; i++)
+//     {
+//         if (diag1[i] < 0)
+//             pos1[i] = diag1[i] + pos1[i];
 
-        if (diag2[i] < 0)
-            pos2[i] = diag2[i] + pos2[i];
-    }
+//         if (diag2[i] < 0)
+//             pos2[i] = diag2[i] + pos2[i];
+//     }
 
-    //check the X axis
-    if(std::abs(pos1[0] - pos2[0]) < (size1[0]))
-    {
-        checkXYZ[0] = true;
-        //check the Y axis
-        if(std::abs(pos1[1] - pos2[1]) < (size1[1]))
-        {
-            checkXYZ[1] = true;
-            //check the Z axis
-            if(std::abs(pos1[2] - pos2[2]) < (size1[2]))
-            {
-                checkXYZ[2] = true;
-            }
-        }
-    }
+//     //check the X axis
+//     if(std::abs(pos1[0] - pos2[0]) < (size1[0]))
+//     {
+//         checkXYZ[0] = true;
+//         //check the Y axis
+//         if(std::abs(pos1[1] - pos2[1]) < (size1[1]))
+//         {
+//             checkXYZ[1] = true;
+//             //check the Z axis
+//             if(std::abs(pos1[2] - pos2[2]) < (size1[2]))
+//             {
+//                 checkXYZ[2] = true;
+//             }
+//         }
+//     }
 
-    return &checkXYZ;
-}
+//     return &checkXYZ;
+// }
 
 // int main()
 // {
--- test.cpp
+++ test.cpp
@@ -19,38 +19,6 @@
 
 int main()
 {
-   
-
-    // std::array<int,3> size{1,2,3};
-    // std::array<int,3> pos{0,0,0};
-    // std::array<bool,3> is_rotatableXYZ{true,true,true};
-
-    // AbstractBox box = AbstractBox(size, is_rotatableXYZ);
-
-    // std::vector<std::array<int,3>> ps{{0,0,0}};
-    // std::array<int,3> cont_size = {10,10,10};
-    // AbstractContainer cont = AbstractContainer{cont_size};
-    // // cont.put(box, pos);
-
-    // std::vector<AbstractBox> boxes;
-    // // boxes.reserve(10);
-
-    // for (int i(0); i<1000; i++)
-    // {
-    //     AbstractBox box = AbstractBox(size, is_rotatableXYZ);
-    //     boxes.push_back(box);
-    // }
-    
-    clock_t tStart = clock();
-    // for (auto i:boxes)
-    // {
-    //     i.rotateX();
-    //     cont.put(i, pos);
-    // }
-    // int vp[3] = { size[0], size[1], size[2]};
-    // for (auto v:vp)
-    //     std::cout<<v;
-
     
     std::array<int,3> pos1{4,4,4};
     std::array<int,3> diag1{5,5,5};
@@ -71,9 +39,11 @@ int main()
     box1.set_diag(diag1);
     box2.set_diag(diag2);
 
+
+    clock_t tStart = clock();
     std::array<bool,3> *b = check_boxes_intersect(box1, box2);
 
-    std::cout << (*b)[0] << std::endl;
+    // std::cout << (*b)[0] << std::endl;
     printf("Time taken: %.10fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     
     return 0;
